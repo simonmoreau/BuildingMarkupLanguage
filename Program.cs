ec06788c56bdf319dd516e32827e9b274a11c8b0
Simon Moreau <simon@bim42.com>
2018-06-19 21:09:51 +0200

    new console dotnet app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildingMarkupLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
